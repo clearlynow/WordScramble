1c5401e65e8761feaad0b0a75345d7eaec76cb76
clearlynow <59779189+clearlynow@users.noreply.github.com>
2021-01-08 10:47:02 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alison Gorman on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
